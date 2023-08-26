3c02d7909347b8e78abcf53f138bb645eb50fb3c
Akbar-07 <116638921+Akbar-07@users.noreply.github.com>
2023-08-26 15:34:43 +0500

    aa
    aa

--- test/src/App.js
+++ test/src/App.js
@@ -8,7 +8,6 @@ function App() {
         <h2>alik</h2>
         <h4>asas</h4>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui, magni?</p>
-        <p>Lorem ipsum dolor sit.</p>
     </div>
   );
 }
